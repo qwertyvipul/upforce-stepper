c4241f84794411994007c1a0e218cec79f9ba591
Vipul Sharma <vipuls526@gmail.com>
2023-02-22 00:33:25 +0530

    Added App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import MyPage from './components/MyPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid display="flex" justifyContent={'center'} container>
+                <Grid item xs={8}>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Paper variant="outlined">
+                            <MyPage />
+                        </Paper>
+                    </Box>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
